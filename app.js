d5698cb91d566b5276239af5d0c11b5da7acf178
Redwood4413 <73351022+Redwood4413@users.noreply.github.com>
2023-02-18 17:51:06 +0100

    Add files via upload

--- app.js
+++ app.js
@@ -1,11 +1,7 @@
 const randomNum = (min, max) => {
   return Math.floor(Math.random() * (max - min) + min);
 }
-const checkForSpecAttack = (previous, ready) => {
-  console.log(previous, ready)
 
-  if (ready) return true;
-}
 const app = Vue.createApp({
   data() {
     return {
@@ -13,12 +9,15 @@ const app = Vue.createApp({
         isEnded: false,
         playerTurn: true,
         round: 1,
+        counter: 3,
       },
       player: {
         health: 100,
         level: 1,
         critChance: randomNum(5, 15),
         isDead: false,
+        isAbleToHeal: false,
+        isAbleToRetreat: false,
       },
       monster: {
         health: 100,
@@ -62,11 +61,12 @@ const app = Vue.createApp({
 
       this.gameState.round++;
       this.monster.health -= damage;
+      if (this.monster.health <= 0) this.monster.isDead = true;
       this.gameState.playerTurn = false;
-      this.monsterAttack();
-
+      if (!this.monster.isDead) this.monsterAttack();
     },
     monsterAttack() {
+      if (this.monster.isDead) return;
       if (this.gameState.playerTurn) return;
       let damage = 0
       let crit = false;
@@ -89,6 +89,48 @@ const app = Vue.createApp({
         }
       }, randomNum(300, 800));
     },
+    restartTheGame() {
+      this.gameState.isEnded = false;
+      this.gameState.playerTurn = true;
+      this.gameState.round = 1;
+
+      this.player.health = 100;
+      this.player.level = 1;
+      this.player.critChance = randomNum(5, 15);
+      this.player.isDead = false;
+
+      this.monster.health = 100;
+      this.monster.level = randomNum(1, 5);
+      this.monster.critChance = randomNum(5, 15);
+      this.monster.isDead = false;
+
+      this.specialAttack.player.ready = false;
+      this.specialAttack.player.previous = 0;
+
+      this.specialAttack.monster.ready = false;
+      this.specialAttack.monster.previous = 0;
+
+      this.battleLog = [];
+    },
+    findNewMonster() {
+      this.gameState.isEnded = false;
+      this.gameState.playerTurn = true;
+      this.gameState.round = 1;
+
+      this.player.health += 40;
+      this.player.level += 1;
+
+      this.monster.health = 100;
+      this.monster.level = randomNum(1, 5);
+      this.monster.critChance = randomNum(5, 15);
+      this.monster.isDead = false;
+
+      this.specialAttack.player.ready = false;
+      this.specialAttack.player.previous = 0;
+
+      this.specialAttack.monster.ready = false;
+      this.specialAttack.monster.previous = 0;
+    },
     launchSpecialAttack() {
       this.playerAttack(15); // call the function with additional damage for special attack
       this.specialAttack.player.ready = false;
@@ -96,17 +138,69 @@ const app = Vue.createApp({
     },
     battlelog(str) {
       this.battleLog.push(str);
+    },
+    healPlayer() {
+      this.gameState.playerTurn = false;
+      const healValue = randomNum(10, 20) + this.player.level * 3;
+      this.player.health += healValue;
+      if (this.player.health >= 100) this.player.health = 100;
+
+      this.battlelog(`You're healing yourself for ${healValue} hp.`)
+      this.monsterAttack();
+    },
+    retreat() {
+      this.gameState.playerTurn = true;
+      this.gameState.isEnded = true;
+
+      const value = randomNum(20, 30);
+      this.monster.health += value
+      this.player.health += 30;
+
+      this.battlelog(`You've retreated, monster heals for ${value}HP, and You for 30HP.`)
+      setInterval(() => {
+        this.gameState.counter--
+
+        if (this.gameState.counter === 0) {
+          this.gameState.isEnded = false;
+          this.gameState.counter = 3;
+          this.gameState.round = 1;
+          return;
+        }
+      }, 1000);
     }
   },
   watch: {
     'specialAttack.player.previous'() {
       if (this.specialAttack.player.previous === 3) this.specialAttack.player.ready = true;
-
-      checkForSpecAttack(this.specialAttack.player.previous, this.specialAttack.player.ready)
+    },
+    'player.health'() {
+      if (this.player.health <= 30) {
+        this.player.isAbleToHeal = true;
+        this.player.isAbleToRetreat = true;
+      } else {
+        this.player.isAbleToHeal = false;
+        this.player.isAbleToRetreat = false;
+      };
+      if (this.player.health <= 0) {
+        this.player.isDead = true;
+        this.gameState.isEnded = true;
+        this.player.health = 0;
+      };
+    },
+    'monster.health'() {
+      if (this.monster.health <= 0) {
+        this.monster.isDead = true;
+        this.gameState.isEnded = true;
+        this.monster.health = 0;
+        console.log(this.monster.isDead)
+      }
     }
   },
   computed: {
     playerHealthBar() {
+      if (this.player.health <= 0) {
+        return { width: `${0}%` };
+      }
       return {
         width: `${this.player.health}%`,
       }
@@ -117,7 +211,7 @@ const app = Vue.createApp({
       }
     },
     playerSpecAttackReady() {
-      return checkForSpecAttack(this.specialAttack.player.previous, this.specialAttack.player.ready)
+      if (this.specialAttack.player.ready) return true;
     },
   }
 })
